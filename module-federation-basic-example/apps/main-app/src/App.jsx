12f242250947822f13ce3ffd8822539ec0fd4044
happycho <zanda33@naver.com>
2024-09-23 07:36:09 +0900

    강의 학습내용 추가

---
+++ module-federation-basic-example/apps/main-app/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { map, join } from "lodash";
+
+import { ErrorBoundary } from "react-error-boundary";
+
+import "./index.css";
+
+const Button = React.lazy(() => import("component_app/Button"));
+
+const App = () => (
+  <div className="container">
+    <div>Name: main-app</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+    <div>{join(map(["1", "2"]), "-")}</div>
+    <ErrorBoundary fallback={<div>Error</div>}>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Button
+          onClick={() => {
+            console.log("Clicked!!");
+          }}
+        >
+          Primary
+        </Button>
+      </React.Suspense>
+    </ErrorBoundary>
+    <ErrorBoundary fallback={<div>Error</div>}>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Button type="warning">Warning</Button>
+      </React.Suspense>
+    </ErrorBoundary>
+  </div>
+);
+
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<App />);
